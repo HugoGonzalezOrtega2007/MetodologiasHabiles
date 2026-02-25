db04e330d4dd04a3f7d7d028effb835bc836f888
Lemsito22 <lemquilca22@gmail.com>
2026-02-25 09:47:12 +0100

    .

--- src/Java/Guerrero.java
+++ src/Java/Guerrero.java
@@ -1,6 +1,28 @@
 package Java;
 
 public class Guerrero extends Combate {
-    private int escudo=50;
-    
+    private int escudo;
+    private int furia;
+
+    public Guerrero(String nombre) {
+        super(nombre);
+        this.escudo = 50;
+        this.furia = 0;
+    }
+
+    public int getEscudo() {
+        return escudo;
+    }
+
+    public void setEscudo(int escudo) {
+        this.escudo = escudo;
+    }
+
+    public int getFuria() {
+        return furia;
+    }
+
+    public void setFuria(int furia) {
+        this.furia = furia;
+    }
 }
