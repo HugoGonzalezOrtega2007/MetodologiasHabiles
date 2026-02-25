f4d267a8bfa8160a16e06877a9539a10d16918c7
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2026-02-25 09:20:22 +0100

    .

--- src/Java/Combate.java
+++ src/Java/Combate.java
@@ -1,6 +1,14 @@
 package Java;
+import java.util.Random;
 
 public class Combate {
     private int vidaMaxima = 100;
     private int ataque = 15;
+    private int esquivo = (int) (Math.random() * 10);
 }
+
+if (esquivo == 3) {
+    sout("El personaje ha esquivado el ataque");
+        } else {
+    sout("El personaje ha recibido el ataque");
+                }
--- src/Java/Mago.java
+++ src/Java/Mago.java
@@ -1,5 +1,6 @@
 package Java;
 
 public class Mago extends Combate {
-
+    private int ataqueMagico = 20;
+    private int
 }
