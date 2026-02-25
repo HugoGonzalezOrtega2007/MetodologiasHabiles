6a93aec0410c9850f61992b4c4148f25dc0024ce
Lemsito22 <lemquilca22@gmail.com>
2026-02-25 09:23:32 +0100

    .

--- src/Java/Combate.java
+++ src/Java/Combate.java
@@ -5,12 +5,14 @@ public class Combate {
     private int vidaMaxima = 100;
     private int ataque = 15;
 
-}
-public void esquivar(){
-    int esquivo = (int) (Math.random() * 10);
-    if (esquivo == 3) {
-        System.out.println("El personaje ha esquivado el ataque");
-    } else {
-        System.out.println("El personaje ha recibido el ataque");
+    public void esquivar(){
+        int esquivo = (int) (Math.random() * 10);
+        if (esquivo == 3) {
+            System.out.println("El personaje ha esquivado el ataque");
+        } else {
+            System.out.println("El personaje ha recibido el ataque");
+        }
     }
+
 }
+
