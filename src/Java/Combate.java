f24b0244dcfb24dee4da540f01574c0a1bc59e55
HugoGonzalezOrtega2007 <gonzalezhugoortega@digitechfp.com>
2026-02-25 09:49:41 +0100

    cambios

--- src/Java/Combate.java
+++ src/Java/Combate.java
@@ -1,5 +1,6 @@
 package Java;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Combate {
     private String nombre;
@@ -7,9 +8,9 @@ public class Combate {
     private int vidaMaxima;
     private int ataque;
 
-    public Combate(){
-
+    public Combate() {
     }
+
     public Combate(String nombre) {
         this.nombre = nombre;
         this.energia = 80;
@@ -20,7 +21,6 @@ public class Combate {
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -28,7 +28,6 @@ public class Combate {
     public int getEnergia() {
         return energia;
     }
-
     public void setEnergia(int energia) {
         this.energia = energia;
     }
@@ -36,7 +35,6 @@ public class Combate {
     public int getVidaMaxima() {
         return vidaMaxima;
     }
-
     public void setVidaMaxima(int vidaMaxima) {
         this.vidaMaxima = vidaMaxima;
     }
@@ -44,7 +42,6 @@ public class Combate {
     public int getAtaque() {
         return ataque;
     }
-
     public void setAtaque(int ataque) {
         this.ataque = ataque;
     }
@@ -58,11 +55,28 @@ public class Combate {
         }
     }
 
-    public void ataque(){
+    public void ataqueEnergiaBaja(){
         if (energia<20){
             vidaMaxima-=ataque+15;
         }
     }
 
+    public void turnoMago() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Que acción deseas realizar: (1. Ataque normal | 2. Ataque magico | 3. Esquivar");
+        int eleccion = sc.nextInt();
+
+        switch (eleccion) {
+            case 1:
+                System.out.println("te he atacadi");
+                vidaMaxima -= 10;
+                break;
+            case 2:
+                break;
+            case 3:
+
+                break;
+        }
+    }
 }
 
--- src/Java/Mago.java
+++ src/Java/Mago.java
@@ -1,6 +1,8 @@
 package Java;
 
+import java.util.Scanner;
+
 public class Mago extends Combate {
     private int ataqueMagicoMago = 20;
-    private int
+
 }
